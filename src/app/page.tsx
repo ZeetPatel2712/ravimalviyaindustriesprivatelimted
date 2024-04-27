09351e7ccda3a6dafae7c6c79717f563beed45e9
nishantlohar007 <flamekaiser9192939495@gmail.com>
2024-04-27 12:23:53 +0530

    h1 created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="h-screen w-full bg-neutral-900">
-            <h1>Hello RMI 💪</h1>
+            <h1 className="text-white">Hello RMI ⚡💪</h1>
         </div>
     );
 }
